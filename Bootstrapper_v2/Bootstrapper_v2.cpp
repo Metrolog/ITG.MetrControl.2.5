5e34cd44eeec32433bcf74b9cd526f274b5890b9
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-12-23 03:04:45 +0300

    * несущественные изменения [ci skip]

--- Bootstrapper_v2/Bootstrapper_v2.cpp
+++ Bootstrapper_v2/Bootstrapper_v2.cpp
@@ -56,10 +56,10 @@ int APIENTRY wWinMain
 	#pragma endregion
 
 	#pragma region Запись файла конфигурации АИС Метрконтроль
-	#define MULTI_LINE_STRING(...) L#__VA_ARGS__
-	CString ConfigFileContent;
+	#define MULTI_LINE_STRINGW(...) L#__VA_ARGS__
+	CStringW ConfigFileContent;
 	ConfigFileContent.FormatMessage(
-		MULTI_LINE_STRING(<?xml version="1.0" encoding="utf-8"?>
+		MULTI_LINE_STRINGW(<?xml version="1.0" encoding="utf-8"?>
 			<entries>
 				<entry key="Key">35AA16F410699668C19C86A111D7A1287FD6D0FB33490C9B</entry>
 				<entry key="IV">3CE48E956DFB399A</entry>
@@ -79,6 +79,8 @@ int APIENTRY wWinMain
 		, (NTLM == _T("yes")) ? _T("true") : _T("false")
 	);
 	MessageBox(NULL, ConfigFileContent, appTitle, MB_OK);
+
+	// https://msdn.microsoft.com/en-us/library/bb762188.aspx
 	#pragma endregion
 
 	//	return ERROR_UNIDENTIFIED_ERROR;
